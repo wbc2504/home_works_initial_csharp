f1267b2dbf3a23a2896c36eb0ad199e5c9324165
unknown <wbc2504@gmail.com>
2025-03-26 23:28:45 -0500

    Ejercicio5

--- Positive_Power/Program.cs
+++ Positive_Power/Program.cs
@@ -1,4 +1,32 @@
-﻿/*Pide al usuario el radio de un círculo y calcula su perímetro. */
-Console.WriteLine("Digita el radio del circulo: ");
-double radio = double.Parse(Console.ReadLine());
-Console.WriteLine($"El perimetro del circulo es: {2*Math.PI*radio}");
+﻿/* Solicita al usuario un número entre 1 y 7 y muestra el día de la
+semana correspondiente, pero solo considerando los días laborables. */
+
+
+Boolean validar = true;
+int num = 0;
+
+while (validar == true){
+Console.WriteLine("Digita un numero entre 1 y 7: ");
+num = int.Parse(Console.ReadLine());
+if (num > 7 || num < 1) 
+{
+    Console.WriteLine("El numero no esta dentro del rango solicitado");
+}
+else 
+{
+    validar =false;
+}
+}
+
+string dia = num switch
+            {
+                1 => "Lunes",
+                2 => "Martes",
+                3 => "Miércoles",
+                4 => "Jueves",
+                5 => "Viernes",
+                _ => "Número inválido. Solo se consideran días laborables (1-5)."
+            };
+Console.WriteLine(dia);
+
+
