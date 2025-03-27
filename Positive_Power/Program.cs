b8dfd27aa11d6a32edf0ed048a632c50583e736a
unknown <wbc2504@gmail.com>
2025-03-26 21:33:11 -0500

    Ejercicio1

---
+++ Positive_Power/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Descripción: Solicita al usuario un número y eleva este número al cuadrado
+solo si es positivo. */
+Console.WriteLine("Digita un numero: ");
+double num = double.Parse(Console.ReadLine());
+if (num > 0) Console.WriteLine($"El cuadrado del numero es: {num * num}");
+else if (num < 0) Console.WriteLine("El numero es negativo");
+else Console.WriteLine("El numero es cero");
