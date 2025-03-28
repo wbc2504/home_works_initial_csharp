6b850b175862f90932da6e312c57cf5a8d1f1f00
unknown <wbc2504@gmail.com>
2025-03-27 19:49:01 -0500

    Ejercicio7

--- Positive_Power/Program.cs
+++ Positive_Power/Program.cs
@@ -1,10 +1,23 @@
-﻿/* Solicita al usuario su salario anual y, si este excede los 12000,
-muestra el impuesto a pagar que es el 15% del excedente.  */
+﻿/* Solicita dos números y muestra el residuo de la división del
+primero entre el segundo.  */
 
-Console.WriteLine("Digita tu salario anual");
-double salario = double.Parse(Console.ReadLine());
-const double tope = 12000;
-if (salario > 12000) Console.WriteLine($"El impuesto a pagar es: {(salario - tope)*0.15}");
-else Console.WriteLine("No aplica para pagar impuestos");
+Console.WriteLine("Digita el primer numero: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digita el segundo numero, debe ser diferente de cero: ");
+int num2 = int.Parse(Console.ReadLine());
+
+while (num2 == 0){
+Console.WriteLine("El segundo numero no puede ser cero, digita otro nuevamente: ");
+num2 = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"El residuo entre el numero {num1} y el numero {num2} es: {num1 % num2}");
+
+
+
+
+
+
+
 
 
